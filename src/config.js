bbf046af695ace901612c8d9702445d42cee4761
Sridhar Duggireddy <sduggireddy@Sridhar-Ds-MBP.lan1>
2020-01-02 22:53:40 -0500

    Setting up our react app

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_Ls62N8bVy    ",
+      APP_CLIENT_ID: "ggtark6asphqiv3ddr0srkvs8",
+      IDENTITY_POOL_ID: "us-east-1:0d210a2a-57db-41dc-9a3e-c8b1a78cc221"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Robpool</h1>
+        <p>Sample Headline</p>
+      </div>
+    </div>
+  );
+}
